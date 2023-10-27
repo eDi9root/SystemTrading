97ead98501c73bfa7743d3357d8e9d7c6488c6e1
eDi9root <119016575+eDi9root@users.noreply.github.com>
2023-10-27 02:52:47 -0400

    Mac to Git

--- api/Kiwoom.py
+++ api/Kiwoom.py
@@ -10,26 +10,43 @@ class Kiwoom(QAxWidget):
         super().__init__()
         self._make_kiwoom_instance()
         self._set_signal_slots()
+        self._comm_connect()
+
+        self.account_number = self.get_account_number()
         # Automatically call once when initialize
 
     def _make_kiwoom_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
         # Update API identifier to function to get Open API Control Function
 
-    def _set_signal_slots(self):
-        self.OnEventConnect.connect(self._login_slot)
-        # Slot function to get a response from API
+    def _set_signal_slots(self): # Slot function to get a response from API
+        self.OnEventConnect.connect(self._login_slot) # Get login response to _login_slot
 
-    def _login_slot(self, err_code):
+    def _login_slot(self, err_code): # Get response of trying login
         if err_code == 0:
             print("JS connected")
         else:
             print("JS NOT connected")
         # Refer from KOA
-        self.login_evel_loop.exit()
+        self.login_evel_loop.exit() # Exit response loop
 
     def _comm_connect(self):  # Login Function 
         self.dynamicCall("CommConnect()")
         self.login_event_loop = QEventLoop()  # Begin waiting response for trying login
         self.login_event_loop.exec_()
 
+    def get_account_number(self, tag="ACCNO"): # Function for getting Account Number
+        account_list = self.dynamicCall("GetLoginInfo(QString)", tag)
+        account_number = account_list.split(';')[0] # Environment for simulate
+        print(account_number)
+        return account_number
+
+    """ 
+    "ACCOUNT_CNT" : Number of own accounts
+    "ACCLIST" or "ACCNO" : List of accounts with ';'
+    "USER_ID" : Return user ID
+    "USER_NAME" : Return user name
+    "GetServerGubun" : Return connected server (1 : Simulated Investment, others : Real world server)
+    "KEY_BSECGB" : Return Whether to disable keyboard security (0 : Normal, 1: Disable)
+    "FIREW_SECGB" : Return Whether Firewall settings (0 : Not set, 1: Set, 2: Disable)
+    """
---
+++ main.py
@@ -0,0 +1,13 @@
+
+from api.Kiwoom import *
+import sys
+
+app = QAppilication(sys.argv)
+kiwoom = Kiwoom()
+#kiwoom.get_account_number()
+app.exec_()
+# Auto login
+
+""" 
+3.7 자동 로그인 설정하기 페이지 확인
+"""
