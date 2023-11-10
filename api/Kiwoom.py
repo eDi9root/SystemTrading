6af399b0819d361e84bca0cbcfc4231eef570f14
eDi9root <119016575+eDi9root@users.noreply.github.com>
2023-11-10 01:41:33 -0500

    Window 1108

--- api/Kiwoom.py
+++ api/Kiwoom.py
@@ -51,3 +51,28 @@ class Kiwoom(QAxWidget):
     "KEY_BSECGB" : Return Whether to disable keyboard security (0 : Normal, 1: Disable)
     "FIREW_SECGB" : Return Whether Firewall settings (0 : Not set, 1: Set, 2: Disable)
     """
+
+    def get_code_list_by_market(self, market_type): # Function for getting code list
+        code_list = self.dynamicCall("GetCodeListByMarket(QString)", market_type)
+        code_list = code_list.split(';')[:-1]
+        return code_list
+
+    '''
+    Get List by Market
+    KOA Developing Guide -> other functions -> Functions related to Stock information
+    KOA의 개발 가이드 -> 기타 함수 -> 종목 정보 관련 함수
+    0: KOSPI
+    10: KOSDAQ
+    3: ELW
+    8: ETF
+    50: KONEX
+    4: Mutual Fund
+    5: New stock warrant
+    6: Ritz
+    9: Haier Fund
+    30: K-OTC
+    '''
+
+    def get_master_code_name(self, code): # Function for return name from code
+        code_name = self.dynamicCall("GetMasterCodeName(QString)", code)
+        return code_name
--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import sys
 
 app = QApplication(sys.argv)
 kiwoom = Kiwoom()
-kiwoom.get_account_number()
+# kiwoom.get_account_number()
+
+Kospi_code_list = kiwoom.get_code_list_by_market("0")  # KOSPI number
+print(Kospi_code_list)
+for code in Kospi_code_list:
+    code_name = kiwoom.get_master_code_name(code)
+    print(code, code_name)
+
+Kosdaq_code_list = kiwoom.get_code_list_by_market("10") # KOSDAP number
+print(Kosdaq_code_list)
+for code in Kosdaq_code_list:
+    code_name = kiwoom.get_master_code_name(code)
+    print(code, code_name)
+
 app.exec_()
 # Auto login
-
-""" 
-3.7 자동 로그인 설정하기 페이지 확인
-"""
