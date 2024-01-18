9f60cf9bfc7ad63ef385e34f332a85f6a0b35b39
eDi9root <119016575+eDi9root@users.noreply.github.com>
2024-01-17 21:16:40 -0500

    Window to Git (code added) 240117

--- api/Kiwoom.py
+++ api/Kiwoom.py
@@ -26,24 +26,27 @@ class Kiwoom(QAxWidget):
         # dictionary of dictionary
         self.balance = {}
         # dictionary containing purchase information for the stock from stock code
+        self.universe_realtime_transaction_info = {}
+        # dictionary containing real time transaction information
 
     def _make_kiwoom_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
         # Update API identifier to function to get Open API Control Function
 
-    def _set_signal_slots(self):  # Slot function to get a response from API
-        self.OnEventConnect.connect(self._login_slot)  # Get login response to _login_slot
+    def _set_signal_slots(self):                                # Slot function to get a response from API
+        self.OnEventConnect.connect(self._login_slot)           # Get login response to _login_slot
         self.OnReceiveTrData.connect(self._on_receive_tr_data)  # Set getting response result from TR
-        self.OnReceiveMsg.connect(self._on_receive_msg)  # Set receiving TR message to _on_receive_msg
+        self.OnReceiveMsg.connect(self._on_receive_msg)         # Set receiving TR message to _on_receive_msg
         self.OnReceiveChejanData.connect(self._on_chejan_slot)  # Set receiving conclusion to _on_chejan_slot
+        self.OnReceiveRealData.connect(self._on_receive_real_data)  # Set receiving real time transaction data
 
-    def _login_slot(self, err_code): # Get response of trying login
+    def _login_slot(self, err_code):  # Get response of trying login
         if err_code == 0:
             print("JS connected")
         else:
             print("JS NOT connected")
         # Refer from KOA
-        self.login_event_loop.exit() # Exit response loop
+        self.login_event_loop.exit()  # Exit response loop
 
     def _comm_connect(self):  # Login Function 
         self.dynamicCall("CommConnect()")
@@ -51,11 +54,12 @@ class Kiwoom(QAxWidget):
         self.login_event_loop = QEventLoop()  # Begin waiting response for trying login
         self.login_event_loop.exec_()
 
-    def get_account_number(self, tag="ACCNO"): # Function for getting Account Number
+    def get_account_number(self, tag="ACCNO"):  # Function for getting Account Number
         account_list = self.dynamicCall("GetLoginInfo(QString)", tag)
-        account_number = account_list.split(';')[0] # Environment for simulate
+        account_number = account_list.split(';')[0]  # Environment for simulate
         print(account_number)
         return account_number
+
     """ 
     "ACCOUNT_CNT" : Number of own accounts
     "ACCLIST" or "ACCNO" : List of accounts with ';'
@@ -66,10 +70,11 @@ class Kiwoom(QAxWidget):
     "FIREW_SECGB" : Return Whether Firewall settings (0 : Not set, 1: Set, 2: Disable)
     """
 
-    def get_code_list_by_market(self, market_type): # Function for getting code list
+    def get_code_list_by_market(self, market_type):  # Function for getting code list
         code_list = self.dynamicCall("GetCodeListByMarket(QString)", market_type)
         code_list = code_list.split(';')[:-1]
         return code_list
+
     '''
     Get List by Market
     KOA Developing Guide -> other functions -> Functions related to Stock information
@@ -86,13 +91,12 @@ class Kiwoom(QAxWidget):
     30: K-OTC
     '''
 
-    def get_master_code_name(self, code): # Function for return name from code
+    def get_master_code_name(self, code):  # Function for return name from code
         code_name = self.dynamicCall("GetMasterCodeName(QString)", code)
         return code_name
 
-
-    def get_price_data(self, code): # Function, retrieves daily information from
-                                    # the stock's listing data to the most recent date
+    def get_price_data(self, code):  # Function, retrieves daily information from
+        # the stock's listing data to the most recent date
         self.dynamicCall("SetInputValue(QString, QString)", "종목코드", code)
         self.dynamicCall("SetInputValue(QString, QString)", "수정주가구분", "1")
         self.dynamicCall("CommRqData(QString, QString, int, QString)", "opt10081_req", "opt10081", 0, "0001")
@@ -157,12 +161,15 @@ class Kiwoom(QAxWidget):
                 code_name = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "종목명")
                 order_number = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "주문번호")
                 order_status = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "주문상태")
-                order_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "주문수량")
+                order_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,
+                                                  "주문수량")
                 order_price = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "주문가격")
                 current_price = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "현재가")
                 order_type = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "주문구분")
-                left_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "미체결수량")
-                executed_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "체결량")
+                left_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,
+                                                 "미체결수량")
+                executed_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,
+                                                     "체결량")
                 ordered_at = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "시간")
                 fee = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "당일매매수수료")
                 tax = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "당일매매세금")
@@ -207,11 +214,15 @@ class Kiwoom(QAxWidget):
                 code = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "종목번호")
                 code_name = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "종목명")
                 quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "보유수량")
-                purchase_price = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "매입가")
-                return_rate = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "수익률(%)")
+                purchase_price = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,
+                                                  "매입가")
+                return_rate = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,
+                                               "수익률(%)")
                 current_price = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i, "현재가")
-                total_purchase_price = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,"매입금액")
-                available_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,"매매가능수량")
+                total_purchase_price = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,
+                                                        "매입금액")
+                available_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString", trcode, rqname, i,
+                                                      "매매가능수량")
 
                 # Data conversion and processing
                 code = code.strip()[1:]
@@ -239,7 +250,6 @@ class Kiwoom(QAxWidget):
         self.tr_event_loop.exit()
         time.sleep(0.5)  # Kiwoom API only allows up to 5 requests per second (0.2 but set 0.5)
 
-
     def get_deposit(self):
         self.dynamicCall("SetInputValue(QString, QString)", "계좌번호", self.account_number)
         self.dynamicCall("SetInputValue(QString, QString)", "비밀번호입력매체구분", "00")
@@ -248,13 +258,15 @@ class Kiwoom(QAxWidget):
 
         self.tr_event_loop.exec_()
         return self.tr_data
+
     # Get the Cash information you have in the account (investment amount)
 
     def send_order(self, rqname, screen_no, order_type, code, order_quantity, order_price, order_classification,
                    origin_order_number=""):
-        order_result = self.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
-                                        [rqname, screen_no, self.account_number, order_type, code, order_quantity,
-                                         order_price, order_classification, origin_order_number])
+        order_result = self.dynamicCall(
+            "SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",
+            [rqname, screen_no, self.account_number, order_type, code, order_quantity,
+             order_price, order_classification, origin_order_number])
         return order_result
 
     '''
@@ -264,9 +276,7 @@ class Kiwoom(QAxWidget):
     3. OnReceiveMsg (Receive order message)
         Function for receiving Korean response (optional function)
     4. OnReceiveChejan (Order reception/conclusion)
-    
     order_classification = 00: Limit order, 03: Market order (only these two can be used in stock market simulator)
-    
     '''
 
     def _on_receive_msg(self, screen_no, rqname, trcode, msg):
@@ -331,3 +341,60 @@ class Kiwoom(QAxWidget):
 
         self.tr_event_loop.exec_()
         return self.tr_data
+
+    def set_real_reg(self, str_screen_no, str_code_list, str_fid_list, str_opt_type):
+        self.dynamicCall("SetRealReg(QString, QString, QString, QString)", str_screen_no,
+                         str_code_list, str_fid_list, str_opt_type)
+        time.sleep(0.5)
+
+    """
+    If register (SetRealReg) the items for which you which to receive real-time trading information,
+    you can receive continuously from the API server
+    """
+
+    def _on_receive_real_data(self, s_code, real_type, real_data):
+        if real_type == "장시작시간":
+            pass
+
+        elif real_type == "주식체결":
+            signed_at = self.dynamicCall("GetCommRealData(QString, int)", s_code, get_fid("체결시간"))
+
+            close = self.dynamicCall("GetCommRealData(QString, int)", s_code, get_fid("현재가"))
+            close = abs(int(close))
+
+            high = self.dynamicCall("GetCommRealData(QString, int)", s_code, get_fid('고가'))
+            high = abs(int(high))
+
+            open = self.dynamicCall("GetCommRealData(QString, int)", s_code, get_fid('시가'))
+            open = abs(int(open))
+
+            low = self.dynamicCall("GetCommRealData(QString, int)", s_code, get_fid('저가'))
+            low = abs(int(low))
+
+            top_priority_ask = self.dynamicCall("GetCommRealData(QString, int)", s_code, get_fid('(최우선)매도호가'))
+            top_priority_ask = abs(int(top_priority_ask))
+
+            top_priority_bid = self.dynamicCall("GetCommRealData(QString, int)", s_code, get_fid('(최우선)매수호가'))
+            top_priority_bid = abs(int(top_priority_bid))
+
+            accum_volume = self.dynamicCall("GetCommRealData(QString, int)", s_code, get_fid('누적거래량'))
+            accum_volume = abs(int(accum_volume))
+
+            # print(s_code, signed_at, close, high, open, low, top_priority_ask, top_priority_bid, accum_volume)
+
+            # If the item code does not exist as a key value in the universe_realtime_transaction_info dictionary,
+            # create it (when receiving real-time data for the item for the first time)
+            if s_code not in self.universe_realtime_transaction_info:
+                self.universe_realtime_transaction_info.update({s_code: {}})
+
+            # Data that continues to be received after initial reception is updated using 'update'.
+            self.universe_realtime_transaction_info[s_code].update({
+                "체결시간": signed_at,
+                "시가": open,
+                "고가": high,
+                "저가": low,
+                "현재가": close,
+                "(최우선)매도호가": top_priority_ask,
+                "(최우선)매수호가": top_priority_bid,
+                "누적거래량": accum_volume
+            })
--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import sys
 app = QApplication(sys.argv)
 kiwoom = Kiwoom()
 
+"""
+
+# kiwoom.set_real_reg("1000", "", get_fid("장운영구분"), "0")
+fids = get_fid("체결시간")
+codes = '005930;007700;000660;'
+kiwoom.set_real_reg("1000", codes, fids, "0")
+"""
+
 '''
 position = kiwoom.get_balance()
 print(position)
