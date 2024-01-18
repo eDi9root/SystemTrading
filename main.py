7641fe43c061c0522e2fddbfb511e86426dae9ad
eDi9root <119016575+eDi9root@users.noreply.github.com>
2024-01-17 20:49:08 -0500

    Window to Git (code added) 240117

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import sys
 app = QApplication(sys.argv)
 kiwoom = Kiwoom()
 
+"""
+orders = kiwoom.get_order()
+print(orders)
+"""
+
+"""
 order_result = kiwoom.send_order('send_buy_order',
                                  '1001', 1,
                                  '007700', 1,
                                  37600, '00')
 
 print(order_result)
-
+"""
 
 # kiwoom.get_account_number()
 
