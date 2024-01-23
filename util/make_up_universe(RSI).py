b5b4421733fbfe659fba9a9f5e56cbc553359644
eDi9root <119016575+eDi9root@users.noreply.github.com>
2024-01-23 01:45:12 -0500

    ADD: db_helper add codes related MySql

--- util/make_up_universe(RSI).py
+++ util/make_up_universe(RSI).py
@@ -12,6 +12,14 @@ CODES = [0, 1]
 # KOSPI:0, KOSDAQ:1
 
 res = requests.get(BASE_URL + str(CODES[0]))
+page_soup = BeautifulSoup(res.text,'lxml')
+# print(page_soup)
+
+total_page_num = page_soup.select_one('td.pgRR > a')
+total_page_num = int(total_page_num.get('href').split('=')[-1])
+print(total_page_num)
+
+
 
 """
 43 pg, 50 each, 2149 total (2024-01-23)
